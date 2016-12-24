c9549399cba30fd6359ed7980fb84438c347b88c
justin seibert <garbageisland@users.noreply.github.com>
2016-12-24 11:17:14 -0800

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,196 @@
+var order = [];
+
+var questions = {
+  {
+    'id'      : 1,
+    'spectrum': 'humble',
+    'question': 'how do you want to be remembered?',
+    'answers' : [
+        {
+            'answer' : 'a',
+            'content': 'steve5.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+            'answer' : 'b',
+            'content': 'steve8.png',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+            'answer' : 'c',
+            'content': 'steve3.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+            'answer' : 'd',
+            'content': 'steve12.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },
+    ],
+  },
+  {
+    'id'      : 2,
+    'spectrum': 'open',
+    'question': 'pretend it\'s candy',
+    'answers' : [
+        {
+          'answer' : 'a',
+            'content': 'bug1.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'b',
+            'content': 'bug6.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'c',
+            'content': 'bug13.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'd',
+            'content': 'bug8.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },
+    ],
+  },
+  {
+    'id'      : 3,
+    'spectrum': '',
+    'question': 'what\'s your idea of carefree fun?',
+    'answers' : [
+        {
+          'answer' : 'a',
+            'content': 'tourist12.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'b',
+            'content': 'tourist9.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'c',
+            'content': 'tourist7.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'd',
+            'content': 'tourist13.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },
+    ],
+  },
+  {
+    'id'      : 4,
+    'spectrum': '',
+    'question': 'who\'s the best poser?',
+    'answers' : [
+        {
+          'answer' : 'a',
+            'content': 'pose4.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'b',
+            'content': 'pose5.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'c',
+            'content': 'pose6.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'd',
+            'content': 'pose3.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },
+    ],
+  },
+  {
+    'id'      : 5,
+    'spectrum': '',
+    'question': 'what\'s your current outlook on the future?',
+    'answers' : [
+        {
+          'answer' : 'a',
+            'content': 'protest1.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'b',
+            'content': 'protest6.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'c',
+            'content': 'protest4.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'd',
+            'content': 'protest5.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },
+    ],
+  },
+  {
+    'id'      : 6,
+    'spectrum': '',
+    'question': 'which candidate is most qualified to lead a nation?',
+    'answers' : [
+        {
+          'answer' : 'a',
+            'content': 'cookie1.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'b',
+            'content': 'cookie5.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'c',
+            'content': 'cookie3.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },{
+          'answer' : 'd',
+            'content': 'cookie4.jpg',
+            'points' : {
+                'default' : 0,
+            },
+        },
+    ],
+  },
+}
