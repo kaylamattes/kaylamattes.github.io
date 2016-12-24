9407a4a6aedd8cdb114a53cf7565d4a9b46dc0e7
justin seibert <garbageisland@users.noreply.github.com>
2016-12-24 11:31:43 -0800

    answers as objects

--- questions.js
+++ questions.js
@@ -1,196 +1,178 @@
 var order = [];
 
 var questions = {
-  {
-    'id'      : 1,
-    'spectrum': 'humble',
+  '1' : {
     'question': 'how do you want to be remembered?',
-    'answers' : [
-        {
-            'answer' : 'a',
-            'content': 'steve5.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-            'answer' : 'b',
-            'content': 'steve8.png',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-            'answer' : 'c',
-            'content': 'steve3.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-            'answer' : 'd',
-            'content': 'steve12.jpg',
-            'points' : {
-                'default' : 0,
-            },
+    'answers' : {
+      'a' : {
+        'content': 'steve5.jpg',
+        'points' : {
+            'default' : 0,
         },
-    ],
+      },
+      'b' : {
+        'content': 'steve8.png',
+        'points' : {
+            'default' : 0,
+        },
+      },
+      'c' : {
+        'content': 'steve3.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+      'd' : {
+        'content': 'steve12.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+    },
   },
-  {
-    'id'      : 2,
-    'spectrum': 'open',
+  '2' : {
     'question': 'pretend it\'s candy',
-    'answers' : [
-        {
-          'answer' : 'a',
-            'content': 'bug1.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'b',
-            'content': 'bug6.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'c',
-            'content': 'bug13.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'd',
-            'content': 'bug8.jpg',
-            'points' : {
-                'default' : 0,
-            },
+    'answers' : {
+      'a' : {
+        'content': 'bug1.jpg',
+        'points' : {
+            'default' : 0,
         },
-    ],
-  },
-  {
-    'id'      : 3,
-    'spectrum': '',
-    'question': 'what\'s your idea of carefree fun?',
-    'answers' : [
-        {
-          'answer' : 'a',
-            'content': 'tourist12.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'b',
-            'content': 'tourist9.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'c',
-            'content': 'tourist7.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'd',
-            'content': 'tourist13.jpg',
-            'points' : {
-                'default' : 0,
-            },
+      },
+      'b' : {
+        'content': 'bug6.jpg',
+        'points' : {
+            'default' : 0,
         },
-    ],
-  },
-  {
-    'id'      : 4,
-    'spectrum': '',
-    'question': 'who\'s the best poser?',
-    'answers' : [
-        {
-          'answer' : 'a',
-            'content': 'pose4.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'b',
-            'content': 'pose5.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'c',
-            'content': 'pose6.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'd',
-            'content': 'pose3.jpg',
-            'points' : {
-                'default' : 0,
-            },
+      },
+      'c' : {
+        'content': 'bug13.jpg',
+        'points' : {
+            'default' : 0,
         },
-    ],
+      },
+      'd' : {
+        'content': 'bug8.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+    },
   },
-  {
-    'id'      : 5,
-    'spectrum': '',
+  '3' : {
+    'question': 'which activity seems least enjoyable?',
+    'answers' : {
+      'a' : {
+        'content': 'tourist12.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+      'b' : {
+        'content': 'tourist9.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+      'c' : {
+        'content': 'tourist7.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+      'd' : {
+        'content': 'tourist13.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+    },
+  },
+  '4' : {
+    'question': 'who is the best poser?',
+    'answers' : {
+      'a' : {
+        'content': 'pose4.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+      'b' : {
+        'content': 'pose5.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+      'c' : {
+        'content': 'pose6.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+      'd' : {
+        'content': 'pose3.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+    },
+  },
+  '5' : {
     'question': 'what\'s your current outlook on the future?',
-    'answers' : [
-        {
-          'answer' : 'a',
-            'content': 'protest1.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'b',
-            'content': 'protest6.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'c',
-            'content': 'protest4.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'd',
-            'content': 'protest5.jpg',
-            'points' : {
-                'default' : 0,
-            },
+    'answers' : {
+      'a' : {
+        'content': 'protest1.jpg',
+        'points' : {
+            'default' : 0,
         },
-    ],
+      },
+      'b' : {
+        'content': 'protest6.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+      'c' : {
+        'content': 'protest4.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+      'd' : {
+        'content': 'protest5.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+    },
   },
-  {
-    'id'      : 6,
-    'spectrum': '',
+  '6' : {
     'question': 'which candidate is most qualified to lead a nation?',
-    'answers' : [
-        {
-          'answer' : 'a',
-            'content': 'cookie1.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'b',
-            'content': 'cookie5.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'c',
-            'content': 'cookie3.jpg',
-            'points' : {
-                'default' : 0,
-            },
-        },{
-          'answer' : 'd',
-            'content': 'cookie4.jpg',
-            'points' : {
-                'default' : 0,
-            },
+    'answers' : {
+      'a' : {
+        'content': 'cookie1.jpg',
+        'points' : {
+          'default' : 0,
         },
-    ],
+      },
+      'b' : {
+        'content': 'cookie5.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+      'c' : {
+        'content': 'cookie3.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+      'd' : {
+        'content': 'cookie4.jpg',
+        'points' : {
+            'default' : 0,
+        },
+      },
+    },
   },
 }
